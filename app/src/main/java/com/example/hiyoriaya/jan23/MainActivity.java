c7fd75ebf07150ae8bea598660035fe4471c9c91
hiyoriaya <hiyoriaya@gmail.com>
2016-01-24 00:13:31 +0900

    写経中

--- app/src/main/java/com/example/hiyoriaya/jan23/MainActivity.java
+++ app/src/main/java/com/example/hiyoriaya/jan23/MainActivity.java
@@ -4,17 +4,31 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
-
-
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+    private EditText mInputMassage;
+    private Button mSendMassage;
+    private LinearLayout mMassageLog;
+    private TextView mCpuMassage;
+    private TextView mUserMassage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mInputMassage = (EditText)findViewById(R.id.input_massage);
+        mSendMassage = (Button)findViewById(R.id.send_massage);
+        mMassageLog = (LinearLayout)findViewById(R.id.massage_log);
+        mCpuMassage = (TextView)findViewById(R.id.cpu_massage);
+        mUserMassage = (TextView)findViewById(R.id.user_massage);
+        mSendMassage.setOnClickListener(this);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -35,5 +49,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.send_massage){
+            Toast.makeText(this,"デデーン",Toast.LENGTH_SHORT).show();
+        }
     }
 }
